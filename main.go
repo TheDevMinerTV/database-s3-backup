c2ac2e474abed0ae7d631e3975896e3f4fc1aa2c
DevMiner <devminer@devminer.xyz>
2023-02-18 20:16:51 +0100

    feat: own scheduler

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
-	_ = godotenv.Load()
+	if err := godotenv.Load(); err != nil {
+		log.Printf("WARNING: Error loading .env file: %s\n", err)
+	}
 
 	postgresHost := flag.String("postgres-host", lookupEnvOrString("POSTGRES_HOST", "localhost"), "connectionOptions host")
 	postgresPort := flag.Int("postgres-port", lookupEnvOrInt("POSTGRES_PORT", 5432), "connectionOptions port")
@@ -24,6 +27,8 @@ func main() {
 	s3Bucket := flag.String("s3-bucket", lookupEnvOrString("S3_BUCKET", "postgres-backups"), "S3 bucket")
 	s3AccessKey := flag.String("s3-access-key", lookupEnvOrString("S3_ACCESS_KEY", "minio"), "S3 access key")
 	s3SecretKey := flag.String("s3-secret-key", lookupEnvOrString("S3_SECRET_KEY", "minioadmin"), "S3 secret key")
+
+	every := flag.Duration("every", lookupEnvOrDuration("EVERY", 24*time.Hour), "How often to run the backup")
 
 	flag.Parse()
 
@@ -40,22 +45,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	file := newFileName(*postgresDB)
-	err = RunDump(&connectionOptions{
-		Host:     *postgresHost,
-		Port:     *postgresPort,
-		Database: *postgresDB,
-		Username: *postgresUser,
-		Password: *postgresPassword,
-	}, file)
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		file := newFileName(*postgresDB)
+		err = RunDump(&connectionOptions{
+			Host:     *postgresHost,
+			Port:     *postgresPort,
+			Database: *postgresDB,
+			Username: *postgresUser,
+			Password: *postgresPassword,
+		}, file)
+		if err != nil {
+			log.Printf("WARNING: Failed to dump database: %s", err)
+		}
 
-	log.Printf("Uploading %s to %s", file, *s3Bucket)
+		log.Printf("Uploading %s to %s", file, *s3Bucket)
 
-	if _, err := s3.FPutObject(context.Background(), *s3Bucket, file, file, minio.PutObjectOptions{}); err != nil {
-		log.Fatal(err)
+		if _, err := s3.FPutObject(context.Background(), *s3Bucket, file, file, minio.PutObjectOptions{}); err != nil {
+			log.Printf("WARNING: Failed to upload %s to %s: %s", file, *s3Bucket, err)
+		}
+
+		log.Printf("Removing %s", file)
+		if err := os.Remove(file); err != nil {
+			log.Printf("WARNING: Failed to remove %s: %s", file, err)
+		}
+
+		log.Printf("Sleeping for %s", *every)
+		time.Sleep(*every)
 	}
 }
 
@@ -70,6 +85,19 @@ func lookupEnvOrString(key string, defaultVal string) string {
 func lookupEnvOrInt(key string, defaultVal int) int {
 	if val, ok := os.LookupEnv(key); ok {
 		parsed, err := strconv.Atoi(val)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return parsed
+	}
+
+	return defaultVal
+}
+
+func lookupEnvOrDuration(key string, defaultVal time.Duration) time.Duration {
+	if val, ok := os.LookupEnv(key); ok {
+		parsed, err := time.ParseDuration(val)
 		if err != nil {
 			log.Fatal(err)
 		}
